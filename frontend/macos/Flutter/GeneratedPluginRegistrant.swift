70d2995e26977e9f6ee1a6b7515eaa3c3e917f5b
kan9667 <kanishkappandey@gmail.com>
2026-08-29 18:40:19 +0530

    fix(frontend): upgrade record package to 7.1.1 to resolve platform build error

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import connectivity_plus
 import file_selector_macos
 import flutter_tts
 import just_audio
-import record_darwin
+import record_macos
 import shared_preferences_foundation
 import sqflite_darwin
 
@@ -20,7 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
